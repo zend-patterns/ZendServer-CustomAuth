8f39ae2d248363652be85a3a958642eb6668ac7c
YonmaN <yonni.m@zend.com>
2012-10-03 15:31:48 +0200

    Change-Id: I22badcbb90b1308b3f986a8be2aaac0a70a03454

---
+++ Module.php
@@ -0,0 +1,25 @@
+<?php
+namespace CustomAuth;
+
+use Zend\Loader\StandardAutoloader;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface as AutoloaderProvider;
+
+/**
+ * 
+ * @author yonni.m@zend.com
+ * 
+ */
+class Module implements AutoloaderProvider {
+
+	public function getAutoloaderConfig() {
+		return array(
+			'Zend\Loader\StandardAutoloader' => array(
+				StandardAutoloader::LOAD_NS => array(
+					__NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__
+				)
+			),
+		);
+	}
+}
+
